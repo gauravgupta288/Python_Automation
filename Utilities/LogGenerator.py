39c3b4e6046fbaaf2cca1f82fc09f18a455018a1
guptagaurav288 <guptagaurav288@gmail.com>
2021-06-09 08:23:47 +0530

    new changes

--- Utilities/LogGenerator.py
+++ Utilities/LogGenerator.py
@@ -8,5 +8,7 @@ class LogGen:
         time = str(datetime.now())
         logging.basicConfig(filename="Logs/log.log")
         logger = logging.getLogger()
-        #logger.setLevel(logging.INFO)
+        logger.setLevel(logging.INFO)
+        print(logger)
+        print("inside logger gen")
         return logger
---
+++ a.java
@@ -0,0 +1,9 @@
+package com.Test;
+
+public class a {
+	public static void main(String[] str)
+	{
+		System.out.println("hello world");
+	}
+	
+}
